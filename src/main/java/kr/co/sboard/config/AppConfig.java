fef937a3fdea4725e37d5af50fed144396cb9c8f
dream1910 <dream1910@naver.com>
2025-09-26 12:21:38 +0900

    add 0.0.1

---
+++ src/main/java/kr/co/sboard/config/AppConfig.java
@@ -0,0 +1,25 @@
+package kr.co.sboard.config;
+
+import kr.co.sboard.intercepter.AppInfoInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    private AppInfo appInfo;
+
+    @Bean
+    public AppInfo getAppInfo() {
+        appInfo = new AppInfo();
+        return appInfo;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AppInfoInterceptor(appInfo));
+    }
+}
--- src/main/java/kr/co/sboard/controller/UserController.java
+++ src/main/java/kr/co/sboard/controller/UserController.java
@@ -1,8 +1,13 @@
 package kr.co.sboard.controller;
 
+import kr.co.sboard.config.AppInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class UserController {
 
@@ -10,14 +15,18 @@ public class UserController {
     public String info(){
         return "user/info";
     }
+    //여기서 interceptor수행
     @GetMapping("/user/login")
     public String login(){
+        //model.addAttribute(appInfo);
         return "user/login";
     }
+
     @GetMapping("/user/register")
     public String register(){
         return "user/register";
     }
+
     @GetMapping("/user/terms")
     public String terms(){
         return "user/terms";
