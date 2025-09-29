c6079562c5ef53deee7d03064a96c0ebd10cd82e
dream1910 <dream1910@naver.com>
2025-09-29 17:11:50 +0900

    add 0.0.3

--- src/main/java/kr/co/sboard/service/EmailService.java
+++ src/main/java/kr/co/sboard/service/EmailService.java
@@ -3,14 +3,12 @@ package kr.co.sboard.service;
 import jakarta.mail.Message;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
-import jakarta.servlet.http.HttpSession;
+import kr.co.sboard.dto.SessionData;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -25,8 +23,7 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String sender;
 
-    @Autowired
-    private HttpSession session;
+    private final SessionData sessionData;
 
     public void sendCode(String receiver){
         MimeMessage message = mailSender.createMimeMessage();
@@ -45,7 +42,7 @@ public class EmailService {
             mailSender.send(message);
 
             //현재 세션 저장(현재 클라이언트)
-            session.setAttribute("sessCode", String.valueOf(code));
+            sessionData.setCode(String.valueOf(code));
 
         }catch (Exception e){
             log.error(e.getMessage());
@@ -55,7 +52,7 @@ public class EmailService {
     public boolean verifyCode(String code){
 
         //현재 세션 코드 가져오기
-        String sessCode = (String) session.getAttribute("sessCode");
+        String sessCode = sessionData.getCode();
 
         if(sessCode.equals(code)){
             return true;
